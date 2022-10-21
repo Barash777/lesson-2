c2505d3bef232e2be46a2cb87f0ffc68f810fab4
barash777 <barash.vertihvost@gmail.com>
2022-10-21 20:51:25 +0300

    feat: add active class to SVGs

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import {HomeSvg} from "./src/icon/homeSvg/homeSvg";
 import {ActivitySvg} from "./src/icon/activitySvg/activitySvg";
 import {SliderSvg} from "./src/icon/sliderSvg/sliderSvg";
@@ -13,22 +12,45 @@ export default function App() {
 
     return (
         <View style={styles.container}>
-            <Text>Lesson 2</Text>
             <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                <TouchableOpacity onPress={() => {
-                    setActiveSvg('home')
-                }} hitSlop={{top: 15, bottom: 15, left: 15, right: 15}}><HomeSvg/></TouchableOpacity>
-                <TouchableOpacity onPress={() => {
-                    setActiveSvg('home')
-                }} hitSlop={{top: 15, bottom: 15, left: 15, right: 15}}><ActivitySvg/></TouchableOpacity>
-                <TouchableOpacity onPress={() => {
-                    setActiveSvg('home')
-                }} hitSlop={{top: 15, bottom: 15, left: 15, right: 15}}><SliderSvg/></TouchableOpacity>
-                <TouchableOpacity onPress={() => {
-                    setActiveSvg('home')
-                }} hitSlop={{top: 15, bottom: 15, left: 15, right: 15}}><CalendarSvg/></TouchableOpacity>
+
+                <TouchableOpacity
+                    style={[styles.item, {backgroundColor: activeSvg === 'home' ? 'purple' : 'white'}]}
+                    onPress={() => {
+                        setActiveSvg('home')
+                    }}
+                    hitSlop={{top: 15, bottom: 15, left: 15, right: 15}}>
+                    <HomeSvg color={activeSvg === 'home' ? 'white' : undefined}/>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={[styles.item, {backgroundColor: activeSvg === 'activity' ? 'purple' : 'white'}]}
+                    onPress={() => {
+                        setActiveSvg('activity')
+                    }}
+                    hitSlop={{top: 15, bottom: 15, left: 15, right: 15}}>
+                    <ActivitySvg color={activeSvg === 'activity' ? 'white' : undefined}/>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={[styles.item, {backgroundColor: activeSvg === 'calendar' ? 'purple' : 'white'}]}
+                    onPress={() => {
+                        setActiveSvg('calendar')
+                    }}
+                    hitSlop={{top: 15, bottom: 15, left: 15, right: 15}}>
+                    <CalendarSvg color={activeSvg === 'calendar' ? 'white' : undefined}/>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={[styles.item, {backgroundColor: activeSvg === 'slider' ? 'purple' : 'white'}]}
+                    onPress={() => {
+                        setActiveSvg('slider')
+                    }}
+                    hitSlop={{top: 15, bottom: 15, left: 15, right: 15}}>
+                    <SliderSvg color={activeSvg === 'slider' ? 'white' : undefined}/>
+                </TouchableOpacity>
+
             </View>
-            <StatusBar style="auto"/>
         </View>
     );
 }
@@ -41,4 +63,12 @@ const styles = StyleSheet.create({
         // alignItems: 'center',
         justifyContent: 'center',
     },
+    item: {
+        // backgroundColor: 'purple',
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 25
+    }
 });
